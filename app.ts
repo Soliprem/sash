f03c3bdfd321b8368901dd44612d813d0bc433bf
Soliprem <franci.solidoro@gmail.com>
2025-01-10 20:26:33 +0100

    added OSDs (adapted from Varda-Theme)

--- app.ts
+++ app.ts
@@ -5,12 +5,20 @@ import MprisPlayers from "./widget/MediaPlayer";
 import NotificationPopups from "./widget/NotificationPopups";
 import Applauncher from "./widget/Applauncher";
 import SystemMenuWindow from "./widget/systemMenu/SystemMenuWindow";
+import {
+  ChargingAlertSound,
+  VolumeAlert,
+  BrightnessAlert,
+} from "./widget/Alerts";
 
 App.start({
   css: style,
   main() {
     App.get_monitors().map(Bar);
     App.get_monitors().map(NotificationPopups);
+    App.get_monitors().map(VolumeAlert);
+    App.get_monitors().map(ChargingAlertSound);
+    App.get_monitors().map(BrightnessAlert);
     MprisPlayers();
     Applauncher();
     SystemMenuWindow();
