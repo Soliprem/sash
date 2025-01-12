3b0414f1da28e9bce258cc7f197dbd148bb347fc
Soliprem <franci.solidoro@gmail.com>
2025-01-12 10:45:09 +0100

    bars get correctly toggled on all monitors by using the request handler for "bars"

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import { App } from "astal/gtk3";
+import { App, Gdk, Gtk } from "astal/gtk3";
 import style from "./style.scss";
 import Bar from "./widget/Bar";
 import MprisPlayers from "./widget/MediaPlayer";
@@ -12,10 +12,30 @@ import {
 } from "./widget/Alerts";
 import Calendar from "./widget/Calendar";
 
+function initializeBars() {
+  const bars = new Map<Gdk.Monitor, Gtk.Widget>();
+
+  // initialize
+  for (const gdkmonitor of App.get_monitors()) {
+    bars.set(gdkmonitor, Bar(gdkmonitor));
+  }
+  return bars;
+}
+
+const bars = initializeBars();
+
 App.start({
   css: style,
   main() {
-    App.get_monitors().map(Bar);
+    App.connect("monitor-added", (_, gdkmonitor) => {
+      bars.set(gdkmonitor, Bar(gdkmonitor));
+    });
+
+    App.connect("monitor-removed", (_, gdkmonitor) => {
+      bars.get(gdkmonitor)?.destroy();
+      bars.delete(gdkmonitor);
+    });
+
     App.get_monitors().map(NotificationPopups);
     App.get_monitors().map(VolumeAlert);
     App.get_monitors().map(ChargingAlertSound);
@@ -24,5 +44,15 @@ App.start({
     Applauncher();
     SystemMenuWindow();
     Calendar();
+  },
+  requestHandler(request: string, res: (response: any) => void) {
+    if (request === "bars") {
+      for (const gdkmonitor of App.get_monitors()) {
+        bars.get(gdkmonitor)?.set_visible(!bars.get(gdkmonitor)?.get_visible());
+      }
+      res("bars toggled");
+    } else {
+      res("command not found");
+    }
   },
 });
